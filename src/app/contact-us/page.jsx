2bbdae90d4768f546dcb97271eda88589c0be1c5
neel-frostleaf <neel.jivani@frostleaf.co>
2026-07-31 09:38:17 +0530

    WD-3146 add contact page file only

---
+++ src/app/contact-us/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Kapuria Developers",
+  description: "The best minds in villa, farmhouse architecture & design.",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1>Contact</h1>
+    </>
+  );
+}
